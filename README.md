4afc9b7218db1f2847203951ff3e1493b3d9ef38
Caleb Bassi <calebjbassi@gmail.com>
2019-06-07 21:48:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# platform-dirs-rs
+
+A small Rust library for obtaining platform dependant directory paths for application and user directories.
+
+Note that the directory paths are not guaranteed to exist.
+
+Allows for specifying if an application is CLI or GUI based, since on macOS, CLI applications are expected to conform to the XDG spec, while GUI applications are expected to conform to the [Standard Directories](https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html#//apple_ref/doc/uid/TP40010672-CH2-SW6) guidelines.
+
+The API is still being flushed out, so feel free to PR any breaking changes or additions.
+
+## Example
+
+```rust
+use platform_dirs::{AppDirs, AppUI, UserDirs};
+
+if let Some(app_dirs) = AppDirs::new(Some("program-name"), AppUI::Graphical)
+    app_dirs.config_dir;
+    // Linux:   /home/alice/.config/program-name
+    // Windows: C:\Users\Alice\AppData\Roaming\program-name
+    // macOS:   /Users/Alice/Library/Application Support/program-name
+}
+
+if let Some(app_dirs) = AppDirs::new::<PathBuf>(None, AppUI::CommandLine) {
+    app_dirs.cache_dir;
+    // Linux:   /home/alice/.cache
+    // Windows: C:\Users\alice\AppData\Local
+    // macOS:   /home/alice/.cache
+}
+
+if let Some(user_dirs) = UserDirs::new() {
+    user_dirs.music_dir;
+    // Linux:   /home/alice/Music
+    // Windows: C:\Users\Alice\Music
+    // macOS:   /Users/Alice/Music
+}
+```
+
+## Installation
+
+Add the following to Cargo.toml:
+
+```toml
+[dependencies]
+platform-dirs = "0.1.0"
+```
---
+++ src/lib.rs
@@ -0,0 +1,182 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
+pub use dirs::home_dir;
+
+#[derive(PartialEq)]
+pub enum AppUI {
+    CommandLine,
+    Graphical,
+}
+
+pub struct AppDirs {
+    pub cache_dir: PathBuf,
+    pub config_dir: PathBuf,
+    pub data_dir: PathBuf,
+    pub state_dir: PathBuf,
+}
+
+pub struct UserDirs {
+    pub desktop_dir: PathBuf,
+    pub document_dir: PathBuf,
+    pub download_dir: PathBuf,
+    pub music_dir: PathBuf,
+    pub picture_dir: PathBuf,
+    pub public_dir: PathBuf,
+    pub video_dir: PathBuf,
+}
+
+pub fn is_absolute_path<P>(path: P) -> Option<PathBuf>
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref();
+    if path.is_absolute() {
+        Some(path.to_path_buf())
+    } else {
+        None
+    }
+}
+
+impl AppDirs {
+    pub fn new<P>(prefix: Option<P>, app_ui: AppUI) -> Option<Self>
+    where
+        P: AsRef<Path>,
+    {
+        if cfg!(target_os = "macos") && app_ui == AppUI::Graphical {
+            if home_dir().is_some() {
+                let cache_dir = dirs::cache_dir().unwrap();
+                let data_dir = dirs::data_dir().unwrap();
+
+                if let Some(prefix) = prefix {
+                    cache_dir.join(&prefix);
+                    data_dir.join(&prefix);
+                }
+
+                let config_dir = data_dir.clone();
+                let state_dir = data_dir.clone();
+
+                Some(AppDirs {
+                    cache_dir,
+                    config_dir,
+                    data_dir,
+                    state_dir,
+                })
+            } else {
+                None
+            }
+        } else if cfg!(target_os = "windows") {
+            if let (Some(_home_dir), Some(data_dir), Some(data_local_dir)) =
+                (home_dir(), dirs::data_dir(), dirs::data_local_dir())
+            {
+                let cache_dir = data_local_dir.clone();
+                let config_dir = data_dir.clone();
+                let data_dir = data_local_dir.clone();
+
+                if let Some(prefix) = prefix {
+                    cache_dir.join(&prefix);
+                    config_dir.join(&prefix);
+                    data_dir.join(&prefix);
+                }
+
+                let state_dir = data_dir.clone();
+
+                Some(AppDirs {
+                    cache_dir,
+                    config_dir,
+                    data_dir,
+                    state_dir,
+                })
+            } else {
+                None
+            }
+        } else if let Some(home_dir) = home_dir() {
+            let cache_dir = env::var_os("XDG_CACHE_HOME")
+                .and_then(is_absolute_path)
+                .unwrap_or_else(|| home_dir.join(".cache"));
+            let config_dir = env::var_os("XDG_CONFIG_HOME")
+                .and_then(is_absolute_path)
+                .unwrap_or_else(|| home_dir.join(".config"));
+            let data_dir = env::var_os("XDG_DATA_HOME")
+                .and_then(is_absolute_path)
+                .unwrap_or_else(|| home_dir.join(".local/share"));
+            let state_dir = env::var_os("XDG_STATE_HOME")
+                .and_then(is_absolute_path)
+                .unwrap_or_else(|| home_dir.join(".local/state"));
+
+            if let Some(prefix) = prefix {
+                cache_dir.join(&prefix);
+                config_dir.join(&prefix);
+                data_dir.join(&prefix);
+                state_dir.join(&prefix);
+            }
+
+            Some(AppDirs {
+                cache_dir,
+                config_dir,
+                data_dir,
+                state_dir,
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl UserDirs {
+    pub fn new() -> Option<Self> {
+        if let Some(home_dir) = home_dir() {
+            Some(UserDirs {
+                desktop_dir: dirs::desktop_dir().unwrap_or_else(|| home_dir.join("Desktop")),
+                document_dir: dirs::document_dir().unwrap_or_else(|| home_dir.join("Documents")),
+                download_dir: dirs::download_dir().unwrap_or_else(|| home_dir.join("Downloads")),
+                music_dir: dirs::audio_dir().unwrap_or_else(|| home_dir.join("Music")),
+                picture_dir: dirs::picture_dir().unwrap_or_else(|| home_dir.join("Pictures")),
+                public_dir: dirs::public_dir().unwrap_or_else(|| home_dir.join("Public")),
+                video_dir: dirs::video_dir().unwrap_or_else(|| {
+                    if cfg!(target_os = "macos") {
+                        home_dir.join("Movies")
+                    } else {
+                        home_dir.join("Videos")
+                    }
+                }),
+            })
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config_dir() {
+        let config_dir = home_dir().unwrap().join(".config");
+        env::set_var("XDG_CONFIG_HOME", &config_dir);
+        let app_dirs = AppDirs::new::<PathBuf>(None, AppUI::CommandLine).unwrap();
+
+        if cfg!(target_os = "macos") {
+            assert!(app_dirs.config_dir == config_dir);
+        } else if cfg!(target_os = "windows") {
+            assert!(app_dirs.config_dir == config_dir);
+        } else {
+            assert!(app_dirs.config_dir == config_dir);
+        }
+    }
+
+    #[test]
+    fn test_music_dir() {
+        let music_dir = home_dir().unwrap().join("Music");
+        let user_dirs = UserDirs::new().unwrap();
+
+        if cfg!(target_os = "macos") {
+            assert!(user_dirs.music_dir == music_dir);
+        } else if cfg!(target_os = "windows") {
+            assert!(user_dirs.music_dir == music_dir);
+        } else {
+            assert!(user_dirs.music_dir == music_dir);
+        }
+    }
+}
